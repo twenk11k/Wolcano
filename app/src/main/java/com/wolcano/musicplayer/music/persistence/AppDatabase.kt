c1656939497b525078297c95a4ca5a1f1eb862fd
Selim Demiral <selimdemiral@hotmail.com>
2021-04-18 18:50:23 +0300

    Add SearchHistory entity to AppDataBase

--- app/src/main/java/com/wolcano/musicplayer/music/persistence/AppDatabase.kt
+++ app/src/main/java/com/wolcano/musicplayer/music/persistence/AppDatabase.kt
@@ -4,11 +4,15 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import com.wolcano.musicplayer.music.model.SearchHistory
 import com.wolcano.musicplayer.music.model.Song
 
-@Database(entities = [Song::class], version = 1)
+@Database(entities = [Song::class, SearchHistory::class], version = 1)
 abstract class AppDatabase : RoomDatabase() {
+
     abstract fun songDao(): SongDao
+
+    abstract fun searchHistoryDao(): SearchHistoryDao
 
     companion object {
 
