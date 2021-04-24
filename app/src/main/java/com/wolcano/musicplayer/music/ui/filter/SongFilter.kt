2e25356328f2e6a9add88deae36ddae1e467df7c
twenk11k <selim_demiral@outlook.com>
2021-04-25 00:47:21 +0300

    Improve song filtering

--- app/src/main/java/com/wolcano/musicplayer/music/ui/filter/SongFilter.kt
+++ app/src/main/java/com/wolcano/musicplayer/music/ui/filter/SongFilter.kt
@@ -6,7 +6,7 @@ import com.wolcano.musicplayer.music.ui.adapter.SongAdapter
 import java.util.*
 import kotlin.collections.ArrayList
 
-class SongFilter(private val songList: MutableList<Song>, private val songAdapter: SongAdapter): Filter() {
+class SongFilter(private val songList: MutableList<Song>, private val songAdapter: SongAdapter) : Filter() {
 
     override fun publishResults(constraint: CharSequence?, results: FilterResults?) {
         songAdapter.setFilterList(results!!.values as ArrayList<Song>)
@@ -14,44 +14,46 @@ class SongFilter(private val songList: MutableList<Song>, private val songAdapte
 
     override fun performFiltering(constraint: CharSequence?): FilterResults {
         val filterResults = FilterResults()
-        if (constraint != null && constraint.isNotEmpty()) {
-            val charSeq1 = constraint.toString().trim { it <= ' ' }
-            val charSeqLower = constraint.toString().trim { it <= ' ' }.toLowerCase()
-            val charSeqUpper = constraint.toString().trim { it <= ' ' }.toUpperCase()
+
+        if (!constraint.isNullOrEmpty()) {
+            val charSeq = constraint.toString().trim { it <= ' ' }
+            val charSeqLower = constraint.toString().trim { it <= ' ' }.toLowerCase(Locale.getDefault())
+            val charSeqUpper = constraint.toString().trim { it <= ' ' }.toUpperCase(Locale.getDefault())
             val charSeqUpperUs = constraint.toString().trim { it <= ' ' }.toUpperCase(Locale.US)
             val charSeqLowerUs = constraint.toString().trim { it <= ' ' }.toLowerCase(Locale.US)
-            val yeniList: MutableList<Song> = ArrayList()
-            for (i in this.songList.indices) {
-                if (this.songList[i].title.trim { it <= ' ' }
-                        .contains(charSeq1) || this.songList[i].artist
-                        .contains(charSeq1) || this.songList[i].album
-                        .contains(charSeq1) || this.songList[i].title.toLowerCase()
-                        .contains(charSeqLower) || this.songList[i].artist.toLowerCase()
-                        .contains(charSeqLower) || this.songList[i].album.toLowerCase()
-                        .contains(charSeqLower) || this.songList[i].title.toUpperCase()
-                        .contains(charSeqUpper) || this.songList[i].artist.toUpperCase()
-                        .contains(charSeqUpper) || this.songList[i].album.toUpperCase()
-                        .contains(charSeqUpper) || this.songList[i].title.toUpperCase(
-                        Locale.US
-                    ).contains(charSeqUpperUs) || this.songList[i].artist.toUpperCase(
-                        Locale.US
-                    ).contains(charSeqUpperUs) || this.songList[i].album.toUpperCase(
-                        Locale.US
-                    ).contains(charSeqUpperUs) || this.songList[i].title.toLowerCase(
-                        Locale.US
-                    ).contains(charSeqLowerUs) || this.songList[i].artist.toLowerCase(
-                        Locale.US
-                    ).contains(charSeqLowerUs) || this.songList[i].album.toLowerCase(
-                        Locale.US
-                    ).contains(charSeqLowerUs)
-                ) yeniList.add(this.songList[i])
+            val result: MutableList<Song> = ArrayList()
+            for (song in songList) {
+                if (song.title.trim { it <= ' ' }
+                                .contains(charSeq) || song.artist
+                                .contains(charSeq) || song.album
+                                .contains(charSeq) || song.title.toLowerCase()
+                                .contains(charSeqLower) || song.artist.toLowerCase()
+                                .contains(charSeqLower) || song.album.toLowerCase()
+                                .contains(charSeqLower) || song.title.toUpperCase()
+                                .contains(charSeqUpper) || song.artist.toUpperCase()
+                                .contains(charSeqUpper) || song.album.toUpperCase()
+                                .contains(charSeqUpper) || song.title.toUpperCase(
+                                Locale.US
+                        ).contains(charSeqUpperUs) || song.artist.toUpperCase(
+                                Locale.US
+                        ).contains(charSeqUpperUs) || song.album.toUpperCase(
+                                Locale.US
+                        ).contains(charSeqUpperUs) || song.title.toLowerCase(
+                                Locale.US
+                        ).contains(charSeqLowerUs) || song.artist.toLowerCase(
+                                Locale.US
+                        ).contains(charSeqLowerUs) || song.album.toLowerCase(
+                                Locale.US
+                        ).contains(charSeqLowerUs)
+                ) result.add(song)
             }
-            filterResults.count = yeniList.size
-            filterResults.values = yeniList
+            filterResults.count = result.size
+            filterResults.values = result
         } else {
             filterResults.count = this.songList.size
             filterResults.values = this.songList
         }
         return filterResults
     }
+
 }
